1f50a1bf242d9232a908d39ec58aefbf45e375eb
AsuJuve <superjuve@outlook.es>
2021-10-22 12:34:10 -0500

    Primer commit

---
+++ multiplos.py
@@ -0,0 +1,9 @@
+numeroCiclos = int(input())
+
+print("Multiplos:")
+
+for i in range(1,numeroCiclos+1):
+	multiploDos = i*2
+	print("\t",multiploDos)
+	
+print("\nFin del ciclo")
---
+++ raiz.py
@@ -0,0 +1,7 @@
+import math
+
+numeroInicial = int(input())
+
+raizCuadrada = math.sqrt(numeroInicial)
+
+print("La raiz cuadrada de:",numeroInicial,"es",raizCuadrada)
